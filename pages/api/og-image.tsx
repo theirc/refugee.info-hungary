d96037d26127347c545b0741b74386cc1a4cfd0a
Emilio Guisolfo <emilioguisolfo@gmail.com>
2023-06-01 19:47:27 -0300

    No image page (#37)
    * feat: added logo svg
    
    * feat: added og image tag
    
    * feat: removed default image
    
    * feat: testing og image
    
    * feat: changed image to png
    
    * feat: added siteurl for image
    
    * feat: testing url
    
    * feat: testing image
    
    * feat: reversed changes
    
    * feat: using vercel og
    
    * feat: update image url
    
    * feat: moved file to pages folder
    
    * feat: modified image
    
    * feat: added og-img
    
    ---------
    
    Co-authored-by: Bianca Chami <b.chami39@gmail.com>

---
+++ pages/api/og-image.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default async function handler() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          background: '#000000',
+          width: '100%',
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          width="1200"
+          height="630"
+          src={`https://hungary.refugee.info/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Frefugeeinfo-hungary-logo.0be5b900.png&w=1920&q=75`}
+          alt="logo"
+        />
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
